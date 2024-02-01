8fec07e0828b296e4dc0b1faa09a836a49cd45cf
Zhenghao Lou <rhzx3519@gmail.com>
2024-02-02 01:38:43 +1100

    fix: login return a user

--- controllers/auth/auth.go
+++ controllers/auth/auth.go
@@ -2,7 +2,8 @@ package auth
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/rhzx3519/auth-server/persistance/user"
+	"github.com/rhzx3519/auth-server/domain"
+	userDB "github.com/rhzx3519/auth-server/persistance/user"
 	"github.com/rhzx3519/auth-server/utils/jwt"
 	"github.com/rhzx3519/auth-server/utils/salt"
 	"net/http"
@@ -34,15 +35,24 @@ type LoginData struct {
 	Password string `json: "password" binding:"required"`
 }
 
+type LoginResponse struct {
+	Email    string `json:"email"`
+	Nickname string `json:"nickname"`
+	Token    string `json:"token"`
+}
+
 func Login(c *gin.Context) {
 	var json LoginData
-	if err := c.ShouldBindJSON(&json); err != nil {
+	var err error
+	if err = c.ShouldBindJSON(&json); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{
 			"error": err.Error(),
 		})
 		return
 	}
-	if _, err := user.GetUserbyEmailAndPassword(json.Email, salt.MD5(json.Password)); err != nil {
+
+	var user domain.User
+	if user, err = userDB.GetUserbyEmailAndPassword(json.Email, salt.MD5(json.Password)); err != nil {
 		c.JSON(http.StatusUnauthorized, gin.H{"status": "unauthorized"})
 		return
 	}
@@ -54,6 +64,6 @@ func Login(c *gin.Context) {
 	c.Writer.Header().Set("Authorization", tokenString)
 	c.JSON(http.StatusOK, gin.H{
 		"message": "You are logged in",
-		"token":   tokenString,
+		"data":    LoginResponse{Email: user.Email, Nickname: user.Nickname, Token: tokenString},
 	})
 }
